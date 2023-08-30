039f8b637b067e43d37fa11f2b72021e45d69124
HasanSani16 <122100851+HasanSani16@users.noreply.github.com>
2023-08-30 15:21:07 +0600

    update

--- task1/EmailService.java
+++
@@ -1,6 +0,0 @@
-public class EmailService {
-    public void sendEmail(String to, String subject, String body)
-    {
-        System.out.println("To: "+ "Subject: " + subject + "Body: " + body);
-    }
-}
--- task1/User.java
+++
@@ -1,7 +0,0 @@
-
-public class user{
-    public void login(String userName, String password)
-    {
-        System.out.println("User Name : "+ userName +"\nPass : " + password );
-    }
-}
